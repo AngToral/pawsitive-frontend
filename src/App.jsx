7469a3eb5e6f83564da02e60837281f1dbf31fbf
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-06-30 12:20:00 +0200

    limpio

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { NotificationProvider } from './context/NotificationContext'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Home from './pages/Home'
-import ProfilePage from './pages/ProfilePage'
-import PostDetailPage from './pages/PostDetailPage'
-import CreatePostPage from './pages/CreatePost'
-import SearchPage from './pages/SearchPage'
-import PrivateRoute from './components/PrivateRoute'
-import Layout from './components/Layout'
-import MessagesPage from './pages/MessagesPage'
 import AppRoutes from './AppRoutes'
 
 function App() {
